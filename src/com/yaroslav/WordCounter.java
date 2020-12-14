ccb8a29be6ee64b83c28ce240fcd98183acc9c4c
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-14 14:37:55 +0200

    Create WordCounter.countWords stub

---
+++ src/com/yaroslav/WordCounter.java
@@ -0,0 +1,11 @@
+package com.yaroslav;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class WordCounter {
+    public static HashMap<String, Integer> countWords(String text, boolean ignoreCase) {
+        HashMap<String, Integer> map = new HashMap<>();
+        return map;
+    }
+}

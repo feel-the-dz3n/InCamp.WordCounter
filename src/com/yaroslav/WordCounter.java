8ce4472c833172eb82d8547b8ea9bca0e63c085d
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-14 14:39:07 +0200

    Implement WordCounter.countWords

--- src/com/yaroslav/WordCounter.java
+++ src/com/yaroslav/WordCounter.java
@@ -6,6 +6,22 @@ import java.util.Locale;
 public class WordCounter {
     public static HashMap<String, Integer> countWords(String text, boolean ignoreCase) {
         HashMap<String, Integer> map = new HashMap<>();
+        String[] words = text.split("\\W+");
+
+        for (var word : words) {
+            var usageCount = 1;
+
+            if (ignoreCase)
+                word = word.toLowerCase(Locale.ROOT);
+
+            // This word is already known, so get
+            // and increase it's usage count:
+            if (map.containsKey(word))
+                usageCount = map.get(word) + 1;
+
+            map.put(word, usageCount);
+        }
+
         return map;
     }
 }
